a4e16e535e33c9329497d5b54a8728c46e9c4e52
Ryan Taylor <ryant@nanofab.utah.edu>
2015-01-21 12:24:37 -0700

    remove ssh config

--- chef/active-cookbook/opencoral/recipes/ssh.rb
+++ chef/active-cookbook/opencoral/recipes/ssh.rb
@@ -1,14 +1,14 @@
-directory "/root/.ssh"
+# directory "/root/.ssh"
 
 # cookbook_file "/root/.ssh/authorized_keys" do
 #   source "id_rsa.pub"
 # end
 
-directory "/home/coral/.ssh" do
-  owner "coral"
-  group "coral"
-  mode "755"
-end
+# directory "/home/coral/.ssh" do
+#   owner "coral"
+#   group "coral"
+#   mode "755"
+# end
 
 # cookbook_file "/home/coral/.ssh/authorized_keys" do
 #   source "id_rsa.pub"
