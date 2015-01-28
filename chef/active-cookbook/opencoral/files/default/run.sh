780fa32cb0571246226228175f919ccc1e5355cc
Ryan Taylor <ryant@nanofab.utah.edu>
2015-01-28 11:41:13 -0700

    move files

--- chef/active-cookbook/opencoral/files/default/run.sh
+++ chef/active-cookbook/opencoral/files/default/run.sh
@@ -3,6 +3,6 @@ service apache2 start
 service dnsmasq start
 service postgresql start
 echo 'nameserver 127.0.0.1' > /etc/resolv.conf
-opencoral start
+/usr/local/coral/sbin/opencoral start
 /usr/local/bin/copy_public_key.sh &
 /usr/sbin/sshd -D
