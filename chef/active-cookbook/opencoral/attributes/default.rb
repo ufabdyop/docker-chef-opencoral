ab34a79def8ccf31d9477220e7cddf8e2279eabe
Ryan Taylor <ryant@nanofab.utah.edu>
2015-01-28 14:32:25 -0700

    bump versions

--- chef/active-cookbook/opencoral/attributes/default.rb
+++ chef/active-cookbook/opencoral/attributes/default.rb
@@ -1,7 +1,7 @@
 default['opencoral']['apiserver_repository'] = 'https://github.com/saltlakeryan/coralapiserver.git'
 default['opencoral']['apiserver_branch'] = 'master'
 default['opencoral']['apiserver_home'] = '/coralapiserver'
-default['opencoral']['apiserver_revision'] = '0.1.28'
+default['opencoral']['apiserver_revision'] = '0.1.29'
 
 default['opencoral']['api_repository'] = 'https://github.com/saltlakeryan/coralapi.git'
 default['opencoral']['api_branch'] = 'master'
