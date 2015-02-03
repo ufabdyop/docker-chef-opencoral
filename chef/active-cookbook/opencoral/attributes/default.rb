12564ca79e7e60378f98c03cced9bacada55a8c9
Ryan Taylor <ryant@nanofab.utah.edu>
2015-02-03 12:28:08 -0700

    updating tags for git repos

--- chef/active-cookbook/opencoral/attributes/default.rb
+++ chef/active-cookbook/opencoral/attributes/default.rb
@@ -1,9 +1,9 @@
-default['opencoral']['apiserver_repository'] = 'https://github.com/saltlakeryan/coralapiserver.git'
+default['opencoral']['apiserver_repository'] = 'https://github.com/ufabdyop/coralapiserver.git'
 default['opencoral']['apiserver_branch'] = 'master'
 default['opencoral']['apiserver_home'] = '/coralapiserver'
-default['opencoral']['apiserver_revision'] = '0.1.29'
+default['opencoral']['apiserver_revision'] = '0.1.30'
 
-default['opencoral']['api_repository'] = 'https://github.com/saltlakeryan/coralapi.git'
+default['opencoral']['api_repository'] = 'https://github.com/ufabdyop/coralapi.git'
 default['opencoral']['api_branch'] = 'master'
 default['opencoral']['api_revision'] = '0.1.40'
 default['opencoral']['api_home'] = '/home/coral/coralapi'
