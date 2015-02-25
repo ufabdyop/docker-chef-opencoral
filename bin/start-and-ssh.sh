c0f70e33b4c41b598eb46c1fd76f1e6a838da34f
Ryan Taylor <saltlakeryan@gmail.com>
2015-02-25 16:04:43 -0700

    add start script

---
+++ bin/start-and-ssh.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+RANDOMSUFFIX=`head /dev/urandom | md5sum | perl -pe 's/(........).*/$1/'`
+KEYDIR=/tmp/coral-container-keys-$RANDOMSUFFIX
+mkdir -p $KEYDIR
+ssh-keygen -f $KEYDIR/id_rsa -N ''
+
+docker rm -f coral-vanilla || true 2> /dev/null 
+docker run -d --name coral-vanilla \
+                -p 2233:22 \
+                -p 80:80 \
+                -p 50000:50000 \
+                -p 50001:50001 \
+                -p 50002:50002 \
+                -p 50003:50003 \
+                -p 50004:50004 \
+                -p 50005:50005 \
+                -p 50006:50006 \
+                -p 50007:50007 \
+                -p 50008:50008 \
+                -p 50009:50009 \
+                -p 50010:50010 \
+                -p 50011:50011 \
+                -v /tmp/coral-container-keys:/coral_public_key  \
+		chef-opencoral-vanilla
+
+echo ssh -Y -i /tmp/coral-container-keys/id_rsa -p 2233 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no coral@localhost
