b119a8f45c3b787defeb96f9c38af0bcac4a0b94
Ryan Taylor <saltlakeryan@gmail.com>
2015-02-26 13:39:55 -0700

    update versions of dependencies

--- chef/active-cookbook/opencoral/attributes/default.rb
+++ chef/active-cookbook/opencoral/attributes/default.rb
@@ -1,9 +1,9 @@
 default['opencoral']['apiserver_repository'] = 'https://github.com/ufabdyop/coralapiserver.git'
 default['opencoral']['apiserver_branch'] = 'master'
 default['opencoral']['apiserver_home'] = '/home/coral/coralapiserver'
-default['opencoral']['apiserver_revision'] = '0.2.9'
+default['opencoral']['apiserver_revision'] = '0.2.10'
 
 default['opencoral']['api_repository'] = 'https://github.com/ufabdyop/coralapi.git'
 default['opencoral']['api_branch'] = 'master'
-default['opencoral']['api_revision'] = '0.2.4'
+default['opencoral']['api_revision'] = '0.2.5'
 default['opencoral']['api_home'] = '/home/coral/coralapi'
