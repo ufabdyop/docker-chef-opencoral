60e5ec34795fcedaee284d36da86ffb5fe266519
Ryan Taylor <saltlakeryan@gmail.com>
2015-02-26 14:27:29 -0700

    modify helper scripts

--- bin/start-and-export.sh
+++ bin/start-and-export.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 RANDOMSUFFIX=`head /dev/urandom | md5sum | perl -pe 's/(........).*/$1/'`
-KEYDIR=/tmp/coral-container-keys-$RANDOMSUFFIX
-mkdir -p $KEYDIR
-ssh-keygen -f $KEYDIR/id_rsa -N ''
+EXPORTDIR=/tmp/coral-export-$RANDOMSUFFIX
+mkdir -p $EXPORTDIR
 
 docker rm -f coral-vanilla 2> /dev/null 
-docker run -d --name coral-vanilla \
+docker run --rm -a stdout --name coral-vanilla \
                 -p 2233:22 \
                 -p 80:80 \
                 -p 50000:50000 \
@@ -21,8 +20,9 @@ docker run -d --name coral-vanilla \
                 -p 50009:50009 \
                 -p 50010:50010 \
                 -p 50011:50011 \
-                -v $KEYDIR:/coral_public_key  \
-		ufabdyop/chef-opencoral-vanilla
-
-echo Run this command after services finish starting (give it about 5 seconds or so):
-echo ssh -Y -i $KEYDIR/id_rsa -p 2233 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no coral@localhost
+                -v $EXPORTDIR:/export  \
+		-e EXPORTDIR=/export \
+		ufabdyop/chef-opencoral-vanilla \
+		/export.sh
+		
+echo Exported to $EXPORTDIR
